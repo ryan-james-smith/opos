0fc23a4fa0bc469e63dfdfee4a9e29d5e19a06ff
Ryan Smith <ryan.smith@cartlin.com>
2023-04-24 23:48:38 +1000

    still one warning fixed some

--- src/main/java/com/unicenta/data/gui/ComboBoxValModel.java
+++ src/main/java/com/unicenta/data/gui/ComboBoxValModel.java
@@ -28,16 +28,16 @@ import javax.swing.*;
  *
  * @author  adrian
  */
-public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel {  
-   
-    private List m_aData;
+public class ComboBoxValModel<E> extends AbstractListModel<E> implements ComboBoxModel<E> {
+
+    private List<E> m_aData;
     private IKeyGetter m_keygetter;
-    private Object m_selected;
-    
+    private E m_selected;
+
     /** Creates a new instance of ComboBoxValModel
      * @param aData
      * @param keygetter */
-    public ComboBoxValModel(List aData, IKeyGetter keygetter) {
+    public ComboBoxValModel(List<E> aData, IKeyGetter keygetter) {
         m_aData = aData;
         m_keygetter = keygetter;
         m_selected = null;
@@ -47,7 +47,7 @@ public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel
      *
      * @param aData
      */
-    public ComboBoxValModel(List aData) {
+    public ComboBoxValModel(List<E> aData) {
         this(aData, KeyGetterBuilder.INSTANCE);
     }
 
@@ -56,21 +56,21 @@ public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel
      * @param keygetter
      */
     public ComboBoxValModel(IKeyGetter keygetter) {
-        this(new ArrayList(), keygetter);
+        this(new ArrayList<>(), keygetter);
     }
 
     /**
      *
      */
     public ComboBoxValModel() {
-        this(new ArrayList(), KeyGetterBuilder.INSTANCE);
+        this(new ArrayList<>(), KeyGetterBuilder.INSTANCE);
     }
-    
+
     /**
      *
      * @param c
      */
-    public void add(Object c) {
+    public void add(E c) {
         m_aData.add(c);
     }
 
@@ -78,7 +78,7 @@ public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel
      *
      * @param c
      */
-    public void del(Object c) {
+    public void del(E c) {
         m_aData.remove(c);
     }
 
@@ -87,19 +87,19 @@ public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel
      * @param index
      * @param c
      */
-    public void add(int index, Object c) {
+    public void add(int index, E c) {
         m_aData.add(index, c);
     }
-    
+
     /**
      *
      * @param aData
      */
-    public void refresh(List aData) {
+    public void refresh(List<E> aData) {
         m_aData = aData;
         m_selected = null;
     }
-    
+
     /**
      *
      * @return
@@ -123,62 +123,60 @@ public class ComboBoxValModel extends AbstractListModel implements ComboBoxModel
             return m_selected.toString();
         }
     }
-    
+
     /**
      *
      * @param aKey
      */
-    public void setSelectedKey(Object aKey) {
+    public void setSelectedKey(E aKey) {
         setSelectedItem(getElementByKey(aKey));
     }
-    
+
     /**
      *
      */
     public void setSelectedFirst() {
         m_selected = (m_aData.isEmpty()) ? null : m_aData.get(0);
     }
-    
+
     /**
      *
      * @param aKey
      * @return
      */
-    public Object getElementByKey(Object aKey) {
+    public E getElementByKey(E aKey) {
         if (aKey != null) {
-            Iterator it = m_aData.iterator();
-            while (it.hasNext()) {
-                Object value = it.next();
+            for (E value : m_aData) {
                 if (aKey.equals(m_keygetter.getKey(value))) {
                     return value;
                 }
-            }           
+            }
         }
         return null;
     }
-    
+
     @Override
-    public Object getElementAt(int index) {
+    public E getElementAt(int index) {
         return m_aData.get(index);
     }
-    
+
     @Override
-    public Object getSelectedItem() {
+    public E getSelectedItem() {
         return m_selected;
     }
-    
+
     @Override
     public int getSize() {
         return m_aData.size();
     }
-    
+
     @Override
     public void setSelectedItem(Object anItem) {
-        
+
         if ((m_selected != null && !m_selected.equals(anItem)) || m_selected == null && anItem != null) {
-            m_selected = anItem;
+            m_selected = (E) anItem;
             fireContentsChanged(this, -1, -1);
         }
     }
-    
+
 }
