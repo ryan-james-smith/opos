50c132393b553bdf42959a4fc981c8a0f08b5034
Ryan Smith <ryan.smith@cartlin.com>
2023-04-25 20:48:02 +1000

    fixed warnings and minor changes

--- src/main/java/com/unicenta/pos/util/ValidateBuilder.java
+++ src/main/java/com/unicenta/pos/util/ValidateBuilder.java
@@ -1,4 +1,3 @@
-
 package com.unicenta.pos.util;
 
 import com.unicenta.basic.BasicException;
@@ -7,122 +6,108 @@ import com.unicenta.format.Formats;
 import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 public class ValidateBuilder {
-    
+
     public static int IS_NOT_EMPTY = 1;
     public static int IS_INT = 2;
     public static int IS_DOUBLE = 3;
     public static int IS_CURRENCY = 4;
     public static int IS_DOUBLE_NULL = 5 ;
-    
-    private List<String> listMessage ;
+
+    private List<String> listMessage = new ArrayList<>();
     private Component parent;
-    
-    public ValidateBuilder(Component _parent) {
-        parent = _parent;
-        listMessage = new ArrayList<>();
+
+    public ValidateBuilder(Component parent) {
+        this.parent = parent;
     }
-    
+
     public void setValidate(Object value , int tipe ,  String message){
         if (value==null){
                 listMessage.add(message);
         }
     }
-    
-    public void setValidate(String value , int tipe ,  String message){
-        if(tipe == IS_NOT_EMPTY){
-            if (value.equals("")){
+
+    public void setValidate(String value, int type, String message) {
+        if (type == IS_NOT_EMPTY) {
+            if (value.isEmpty()) {
                 listMessage.add(message);
             }
-        }else if (tipe == IS_INT){
-            if (!isInt(value)){
+        } else if (type == IS_INT) {
+            if (!isInt(value)) {
                 listMessage.add(message);
             }
-        }else if (tipe == IS_DOUBLE){
-            if (!isDouble(value)){
+        } else if (type == IS_DOUBLE) {
+            if (!isDouble(value)) {
                 listMessage.add(message);
             }
-        }else if (tipe == IS_CURRENCY){
-             if (!isCurrency(value)){
-                  listMessage.add(message);
-             }
-        }else if (tipe == IS_DOUBLE_NULL){
-            if (!isDoubleNull(value)){
+        } else if (type == IS_CURRENCY) {
+            if (!isCurrency(value)) {
+                listMessage.add(message);
+            }
+        } else if (type == IS_DOUBLE_NULL) {
+            if (!isDoubleNull(value)) {
                 listMessage.add(message);
             }
         }
     }
-    
+
     private void showMessage(){
-        StringBuilder sb = new StringBuilder();    
+        StringBuilder sb = new StringBuilder();
         sb.append("<br>");
         for (String message : listMessage){
             sb.append(message+"<br>");
         }
             MessageInf msg = new MessageInf(MessageInf.SGN_NOTICE,sb.toString() , null);
             msg.show(parent);
-         
+
     }
-            
+
     public boolean  getValid(){
         boolean isValid = true;
-        if (listMessage.size()>0){
-            isValid= false;
+        if (!listMessage.isEmpty()) {
+            isValid = false;
             showMessage();
         }
         return isValid;
     }
-    
+
     private  boolean isInt(String n) {
             try {
-            Object a = Formats.INT.parseValue(n);
-            if (a!=null){
-                return true;
-            }else{
-                return false;
-            }
+            Optional<Integer> a = Optional.ofNullable(Formats.INT.parseValue(n)).map(Integer.class::cast);
+            return a.isPresent();
         } catch (BasicException ex) {
             return false;
         }
     }
-    
-    
+
+
     private  boolean isDouble(String n) {
             try {
-            Object a = Formats.DOUBLE.parseValue(n);
-            if (a!=null){
-                return true;
-            }else{
-                return false;
-            }
+            Optional<Double> a = Optional.ofNullable(Formats.DOUBLE.parseValue(n)).map(Double.class::cast);
+            return a.isPresent();
         } catch (BasicException ex) {
             return false;
         }
     }
-    
-    
-    
+
+
+
       private  boolean isDoubleNull(String n) {
             try {
-            Object a = Formats.DOUBLE.parseValue(n);
-             return true;   
+            Formats.DOUBLE.parseValue(n);
+             return true;
         } catch (BasicException ex) {
             return false;
         }
     }
-    
-    
+
+
     private boolean isCurrency(String n){
         try {
-            Object a = Formats.CURRENCY.parseValue(n);
-            if (a!=null){
-                return true;
-            }else{
-                return false;
-            }
-            
+            Optional<Double> a = Optional.ofNullable(Formats.CURRENCY.parseValue(n)).map(Double.class::cast);
+            return a.isPresent();
         } catch (BasicException ex) {
             return false;
         }
