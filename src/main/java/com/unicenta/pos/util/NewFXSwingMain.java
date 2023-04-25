954a8ab177eb45215f23caaacfb2179032e3a03c
Ryan Smith <ryan.smith@cartlin.com>
2023-04-25 13:54:03 +1000

    removed deprecated JApplet to now use just JFrame.

--- src/main/java/com/unicenta/pos/util/NewFXSwingMain.java
+++ src/main/java/com/unicenta/pos/util/NewFXSwingMain.java
@@ -11,14 +11,12 @@ import java.awt.Dimension;
 import com.formdev.flatlaf.FlatLightLaf;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -28,8 +26,8 @@ import javax.swing.UIManager;
  * @author user
  */
 @Slf4j
-public class NewFXSwingMain extends JApplet {
-    
+public class NewFXSwingMain extends JFrame {
+
     private static final int JFXPANEL_WIDTH_INT = 300;
     private static final int JFXPANEL_HEIGHT_INT = 250;
     private static JFXPanel fxContainer;
@@ -38,61 +36,37 @@ public class NewFXSwingMain extends JApplet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                try {
-                    UIManager.setLookAndFeel(new FlatLightLaf());
-                } catch (Exception e) {
-                    log.error("Error setting LAF "+e.getMessage());
-                }
-                
-                JFrame frame = new JFrame("JavaFX 2 in Swing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                JApplet applet = new NewFXSwingMain();
-                applet.init();
-                
-                frame.setContentPane(applet.getContentPane());
-                
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                
-                applet.start();
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            } catch (Exception e) {
+                log.error("Error setting LAF "+e.getMessage());
             }
+
+            JFrame frame = new NewFXSwingMain();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
     }
-    
-    @Override
-    public void init() {
+
+    public NewFXSwingMain() {
         fxContainer = new JFXPanel();
         fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
         add(fxContainer, BorderLayout.CENTER);
         // create JavaFX scene
-        Platform.runLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                createScene();
-            }
-        });
+        Platform.runLater(() -> createScene());
     }
-    
+
     private void createScene() {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        btn.setOnAction(event -> System.out.println("Hello World!"));
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         fxContainer.setScene(new Scene(root));
     }
-    
+
 }
